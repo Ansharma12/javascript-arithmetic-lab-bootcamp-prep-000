5407c171c35442be589b9e5f9d92f7f81708cbca
A M <anupreisha@gmail.com>
2018-02-22 03:32:37 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,34 @@
+function add(a, b) {
+return a + b
+}
+
+
+function subtract(a, b) {
+return a - b
+}
+
+
+function multiply(a, b) {
+return a * b
+}
+
+
+function divide(a,b) {
+return a/b
+}
+
+
+function divideBy3(a,b) {
+return a /= b
+}
+
+
+function inc(number){
+ return number + 1
+}
+
+function dec(number){
+return number - 1
+}
+
+function parseInt(2, 10)
